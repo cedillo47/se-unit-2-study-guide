8bafe9a18af518587468e987029862ebd8a45aad
leo <cedillo47@gmail.com>
2021-11-14 00:26:13 +0000

    first attemp

--- test.js
+++ test.js
@@ -1,6 +1,39 @@
+//takes in a sting and doubles every character that is not a vowel, diget,punctoation or whitespace
 
-const arry = [];
+/*
+1.first create a function that take in one peramiter 
+2. decalre an empty let varable, here we will store new string 
+3. write a for loop to loop through every character
+4. create a statment to check if charachter is a vowel or diget or punctioatio or white space. 
+5. if the charater is one of these do nothing 
+6. if it isnt, double that character 
+7. return that let varaibe that we declared ealryer to hold new value
 
-  const isArryEmpty = arry.length ? "not empty" : "empty"; 
-  
-console.log(isArryEmpty)
+*/
+
+function doubleConsonants(string){
+  let double = "";
+
+for(let charachter of string){
+  if (parseInt(charachter, 10)){
+    double = double + charachter
+  }
+  else if(charachter === " "){
+    double = double + charachter
+  }
+  else if(charachter === "!" || charachter === "-"){
+    double = double + charachter
+  }
+  else if(charachter === 'a' || charachter === 'e' || charachter === 'i' || charachter === 'o' || charachter === 'u' || false){
+     double = double + charachter
+  }
+  else if (charachter === 'A' || charachter === 'E' || charachter === 'I' || charachter === 'O' || charachter === 'U' || false){
+    double = double + charachter
+  }
+  else{
+    double = double + charachter + charachter 
+  }
+ } return double
+}
+
+console.log(doubleConsonants("Aplle"))
