d079dd23bcbdcc164695146eb74d35307a07c8d0
leo <cedillo47@gmail.com>
2021-11-14 01:00:45 +0000

    solved question 3

--- test.js
+++ test.js
@@ -46,4 +46,25 @@ function reverse(string){
   return reversed
 }
 
-console.log(reverse('factor In spaces'))
+function isLeapYear(year){
+  if(year % 4 === 0 && year % 100 !== 0 ){
+   return  true
+  } 
+  else if (year % 400 === 0){
+    return true
+  }
+  else{
+   return false
+
+  }
+}
+
+
+
+
+console.log(isLeapYear(1997))
+console.log(isLeapYear(1996))
+console.log(isLeapYear(1900))
+console.log(isLeapYear(2000))
+
+
